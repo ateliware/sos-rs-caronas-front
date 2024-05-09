3e7706ca580012fa91bbc34498a482c109dd90fb
williamsimionatto <william.sn0226@gmail.com>
2024-05-09 14:37:58 -0300

    fix: adjust column width on LoginPage

--- src/pages/Vehicle/VehicleForm.tsx
+++ src/pages/Vehicle/VehicleForm.tsx
@@ -37,108 +37,115 @@ export default function VehicleFormPage() {
 
   return (
     <>
-      <div className="p-s-100">
-        <PageHeader title="Cadastrar Veículo" backButton={true}></PageHeader>
-      </div>
+      <div className="bg-neutral-95 row justify-center">
+        <div className="col-sm-12 col-md-6">
+          <div className="p-s-100">
+            <PageHeader
+              title="Cadastrar Veículo"
+              backButton={true}
+            ></PageHeader>
+          </div>
 
-      <div className="d-flex justify-center" style={{ height: '100vh' }}>
-        <div className="d-flex justify-center w-75">
-          <form
-            autoComplete="off"
-            className="form-max-height w-100 p-s-100"
-            onSubmit={handleSubmit((data) => {
-              onSubmit(data as VehicleFormParams);
-            })}
-          >
-            <Input
-              className="mb-s-200 mt-s-200"
-              form={register('cnh', {
-                required: 'Obrigatório',
-                pattern: {
-                  value: /^[0-9]{11}$/,
-                  message: 'CNH inválida',
-                },
-              })}
-              label="CNH"
-              placeholder="CNH - Apenas números"
-              error={!!errors.cnh}
-              caption={errors.cnh?.message as string}
-              mask="onlyNumbers"
-            />
-            <Input
-              className="mb-s-200 mt-s-200"
-              form={register('brand', { required: 'Obrigatório' })}
-              label="Marca do carro"
-              placeholder="Marca do carro"
-              error={!!errors.brand}
-              caption={errors.brand?.message as string}
-            />
-            <Input
-              className="mb-s-200 mt-s-200"
-              form={register('model', { required: 'Obrigatório' })}
-              label="Modelo do carro"
-              placeholder="Modelo do carro"
-              error={!!errors.model}
-              caption={errors.model?.message as string}
-            />
-            <Input
-              className="mb-s-200 mt-s-200"
-              form={register('color', { required: 'Obrigatório' })}
-              label="Cor do carro"
-              placeholder="Cor do carro"
-              error={!!errors.color}
-              caption={errors.color?.message as string}
-            />
+          <div className="d-flex justify-center" style={{ height: '100vh' }}>
+            <div className="d-flex justify-center w-75">
+              <form
+                autoComplete="off"
+                className="form-max-height w-100 p-s-100"
+                onSubmit={handleSubmit((data) => {
+                  onSubmit(data as VehicleFormParams);
+                })}
+              >
+                <Input
+                  className="mb-s-200 mt-s-200"
+                  form={register('cnh', {
+                    required: 'Obrigatório',
+                    pattern: {
+                      value: /^[0-9]{11}$/,
+                      message: 'CNH inválida',
+                    },
+                  })}
+                  label="CNH"
+                  placeholder="CNH - Apenas números"
+                  error={!!errors.cnh}
+                  caption={errors.cnh?.message as string}
+                  mask="onlyNumbers"
+                />
+                <Input
+                  className="mb-s-200 mt-s-200"
+                  form={register('brand', { required: 'Obrigatório' })}
+                  label="Marca do carro"
+                  placeholder="Marca do carro"
+                  error={!!errors.brand}
+                  caption={errors.brand?.message as string}
+                />
+                <Input
+                  className="mb-s-200 mt-s-200"
+                  form={register('model', { required: 'Obrigatório' })}
+                  label="Modelo do carro"
+                  placeholder="Modelo do carro"
+                  error={!!errors.model}
+                  caption={errors.model?.message as string}
+                />
+                <Input
+                  className="mb-s-200 mt-s-200"
+                  form={register('color', { required: 'Obrigatório' })}
+                  label="Cor do carro"
+                  placeholder="Cor do carro"
+                  error={!!errors.color}
+                  caption={errors.color?.message as string}
+                />
 
-            <FileUpload
-              label="Anexar foto da CNH"
-              uploadPreview={true}
-              accept="image/*"
-              multiple={false}
-              maxSizeInBytes={2097152}
-              onChange={([file]) => {
-                if (file) {
-                  setFileError('');
-                  setFile(file);
-                }
-              }}
-            />
+                <FileUpload
+                  label="Anexar foto da CNH"
+                  uploadPreview={true}
+                  accept="image/*"
+                  multiple={false}
+                  maxSizeInBytes={2097152}
+                  onChange={([file]) => {
+                    if (file) {
+                      setFileError('');
+                      setFile(file);
+                    }
+                  }}
+                />
 
-            {fileError && (
-              <p className="text-left text-negative pt-s-100 font-s-100">
-                {fileError}
-              </p>
-            )}
+                {fileError && (
+                  <p className="text-left text-negative pt-s-100 font-s-100">
+                    {fileError}
+                  </p>
+                )}
 
-            <hr className="mt-s-400 w-100 bg-neutral-60" />
+                <hr className="mt-s-400 w-100 bg-neutral-60" />
 
-            <div className="d-flex justify-between mt-s-400">
-              <div className="col-sm-5">
-                <Button
-                  type="button"
-                  className="!w-100 mb-s-100"
-                  alignText="center"
-                  size="small"
-                  design="transparent"
-                  onClick={() => navigate('/home')}
-                >
-                  Cancelar
-                </Button>
-              </div>
+                <div className="d-flex justify-between mt-s-400">
+                  <div className="col-sm-5">
+                    <Button
+                      type="button"
+                      className="!w-100 mb-s-100"
+                      alignText="center"
+                      size="small"
+                      design="transparent"
+                      onClick={() => navigate('/home')}
+                    >
+                      Cancelar
+                    </Button>
+                  </div>
 
-              <div className="col-sm-5">
-                <Button
-                  type="submit"
-                  isLoading={isSubmitting}
-                  className="!w-100 mb-s-100"
-                  alignText="center"
-                  size="small"
-                >
-                  Confirmar
-                </Button>
-              </div>
+                  <div className="col-sm-5">
+                    <Button
+                      type="submit"
+                      isLoading={isSubmitting}
+                      className="!w-100 mb-s-100"
+                      alignText="center"
+                      size="small"
+                    >
+                      Confirmar
+                    </Button>
+                  </div>
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </>
