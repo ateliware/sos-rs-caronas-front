187dbaa647b9cdfadd615478803902a3c30c2483
williamsimionatto <william.sn0226@gmail.com>
2024-05-09 15:27:30 -0300

    refactor: adjust LoginPage layout
    The LoginPage component has been refactored to adjust the layout and improve the alignment of its elements.

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -29,75 +29,71 @@ export default function LoginPage() {
   }) as SubmitHandler<FieldValues>;
 
   return (
-    <div className="row justify-center">
-      <div className="col-sm-12 col-md-6">
-        <div className="d-flex justify-center">
-          <div className="d-flex justify-center background-login-page">
-            <form
-              autoComplete="off"
-              className="form-max-height w-100"
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <img src={logo} alt="logo" width="100%" />
+    <div className="d-flex justify-center">
+      <div className="d-flex justify-center background-login-page">
+        <form
+          autoComplete="off"
+          className="form-max-height w-100"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <img src={logo} alt="logo" width="100%" />
 
-              <div className="mb-s-350">
-                <p className="font-weight-bold line-height-small text-primary">
-                  Informe o seu CPF, senha e clique em Entrar
-                </p>
-              </div>
-
-              <Input
-                className="mb-s-200"
-                form={register('cpf', {
-                  required: 'Obrigatório',
-                  ...formCpfPattern,
-                })}
-                placeholder="Digite o seu CPF"
-                error={!!errors.cpf}
-                caption={errors.cpf?.message as string}
-              />
-
-              <PasswordInput
-                className="text-primary"
-                form={register('password', { required: 'Obrigatório' })}
-                placeholder="Insira sua senha"
-                error={!!errors.password}
-                caption={errors.password?.message as string}
-              />
-
-              <div className="mt-s-200 d-flex justify-end">
-                <a
-                  href="/recover_password"
-                  className="font-s-150 font-weight-regular line-height-medium text-primary text-decoration-underline"
-                >
-                  Esqueci minha senha
-                </a>
-              </div>
-
-              <Button
-                type="submit"
-                isLoading={isLoadingRequest}
-                className="!w-100 mt-s-750 mb-s-100"
-                alignText="center"
-              >
-                Entrar
-              </Button>
-
-              <div className="mt-s-400 d-flex col justify-center flex-direction-column">
-                <p className="font-weight-bold line-height-small text-primary">
-                  Não possui uma conta?
-                </p>
-
-                <a
-                  className="mt-s-200 font-s-150 font-weight-bold line-height-medium text-primary"
-                  href="/signup"
-                >
-                  Cadastrar-se
-                </a>
-              </div>
-            </form>
+          <div className="mb-s-350">
+            <p className="font-weight-bold line-height-small text-primary">
+              Informe o seu CPF, senha e clique em Entrar
+            </p>
           </div>
-        </div>
+
+          <Input
+            className="mb-s-200"
+            form={register('cpf', {
+              required: 'Obrigatório',
+              ...formCpfPattern,
+            })}
+            placeholder="Digite o seu CPF"
+            error={!!errors.cpf}
+            caption={errors.cpf?.message as string}
+          />
+
+          <PasswordInput
+            className="text-primary"
+            form={register('password', { required: 'Obrigatório' })}
+            placeholder="Insira sua senha"
+            error={!!errors.password}
+            caption={errors.password?.message as string}
+          />
+
+          <div className="mt-s-200 d-flex justify-end">
+            <a
+              href="/recover_password"
+              className="font-s-150 font-weight-regular line-height-medium text-primary text-decoration-underline"
+            >
+              Esqueci minha senha
+            </a>
+          </div>
+
+          <Button
+            type="submit"
+            isLoading={isLoadingRequest}
+            className="!w-100 mt-s-750 mb-s-100"
+            alignText="center"
+          >
+            Entrar
+          </Button>
+
+          <div className="mt-s-400 d-flex col justify-center flex-direction-column">
+            <p className="font-weight-bold line-height-small text-primary">
+              Não possui uma conta?
+            </p>
+
+            <a
+              className="mt-s-200 font-s-150 font-weight-bold line-height-medium text-primary"
+              href="/signup"
+            >
+              Cadastrar-se
+            </a>
+          </div>
+        </form>
       </div>
     </div>
   );
